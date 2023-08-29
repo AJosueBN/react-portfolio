03a56a2d8cac295f066ea9f29a9ab3813808c455
AJosueBN <anthonyjosue.bn@gmail.com>
2023-08-29 16:12:58 +0000

    setup unstyled header to pplay with page choosing

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./style.css"
+
+function Header() {
+  return (
+    <header>
+        <h1>Josue B-N</h1>
+        <nav>
+          <ul>
+          <li>About</li>
+          <li>Portfolio</li>
+          <li>Resume</li>
+          <li>Contact</li>
+          </ul>
+        </nav>
+
+    </header>
+  )
+}
+
+export default Header
