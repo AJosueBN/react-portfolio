ac1adb7ae0e67a71288f1e735a0cbacf98e459ee
AJosueBN <anthonyjosue.bn@gmail.com>
2023-08-29 16:24:30 +0000

    setting up app.js to use appContext to create global variables to the application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import {useState, createContext} from "react"
 import Header from "./components/Header"
+import About from "./pages/About"
+import Portfolio from "./pages/Portfolio"
+import Resume from "./pages/Resume"
+import Contact from "./pages/Contact"
+
+export const AppContext = createContext()
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("About")
+  let content;
+
+  if(currentPage === "About") {
+    content =  <About />
+  }
+  if(currentPage === "Portfolio") {
+    <Portfolio />
+  }
+  if(currentPage === "Resume") {
+    content =  <Resume />
+  }
+  if(currentPage === "Contact") {
+    content =  <Contact />
+  }
+
   return (
     <div className="App">
+      <AppContext.Provider value={{currentPage, setCurrentPage}}>
       <Header />
+      <main>
+      {content}
+      </main>
+      </AppContext.Provider>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,15 +2,20 @@ import React from 'react'
 import "./style.css"
 
 function Header() {
+
+  const handlePageChange = () => {
+    console.log("working")
+  }
+
   return (
     <header>
         <h1>Josue B-N</h1>
         <nav>
           <ul>
-          <li>About</li>
-          <li>Portfolio</li>
-          <li>Resume</li>
-          <li>Contact</li>
+          <li onClick={handlePageChange}>About</li>
+          <li onClick={handlePageChange}>Portfolio</li>
+          <li onClick={handlePageChange}>Resume</li>
+          <li onClick={handlePageChange}>Contact</li>
           </ul>
         </nav>
 
