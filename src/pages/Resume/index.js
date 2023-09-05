4aff1bdfae8d50bfd623265156ed27a3c0410e3c
AJosueBN <anthonyjosue.bn@gmail.com>
2023-09-05 21:52:01 +0000

    rendered resume onto page

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import "./style.css"
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import PDFFile from '../../assets/react-resume.pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function Resume() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
   return (
     <section className='resume'>
-        <h1>Resume</h1>
-        <div></div>
-        </section>
-  )
+      <h1>Resume</h1>
+      <div>
+        <Document file={PDFFile} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+      </div>
+    </section>
+  );
 }
 
-export default Resume
+
+export default Resume;
