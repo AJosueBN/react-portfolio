e1a422258bf2fb8b8b40c672b0e7a630451c6f0f
AJosueBN <anthonyjosue.bn@gmail.com>
2023-09-06 16:27:10 +0000

    Able to download pdf file

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import "./style.css"
 import { Document, Page, pdfjs } from 'react-pdf';
 import PDFFile from '../../assets/react-resume.pdf';
+import { PDFDownloadLink } from '@react-pdf/renderer';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
@@ -12,17 +14,24 @@ function Resume() {
     setNumPages(numPages);
   };
 
+  const downloadPdf = () => {
+    const link = document.createElement('a');
+    link.href = PDFFile;
+    link.download = 'resume.pdf';
+    link.click();
+  };
+
   return (
     <section className='resume'>
       <h1>Resume</h1>
-      <div>
+      <div className="pdf-file">
         <Document file={PDFFile} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber} />
+          <Page size={{ width: 210, height: 297 }} pageNumber={pageNumber} width={600} />
         </Document>
       </div>
+      <button onClick={downloadPdf}>Download PDF</button>
     </section>
   );
 }
-
 
 export default Resume;
